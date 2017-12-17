af32f6161eec4a994dcd9be46be738b1173ac408
gbosnjak <bosnjak.gabrijela@gmail.com>
2017-12-17 11:36:49 +0100

    Move routes from index to batches.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
-const { Batch } = require('./models')
 const { User } = require('./models')
 const passport = require('./config/auth')
 const bodyParser = require('body-parser')
-const users = require('./routes/users')
-const sessions = require('./routes/sessions')
+const {users, sessions, batches} = require('./routes')
 const cors = require('cors')
 
 const PORT = process.env.PORT || 3030
@@ -17,29 +15,9 @@ app
   .use(bodyParser.json())
   .use(passport.initialize())
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!')
-})
-
-app.get('/batches', (req, res, next) => {
-  Batch.find()
-    .then((batches) => res.json(batches))
-    .catch((error) => next(error))
-})
-
-app.post('/batches',
-    passport.authorize('jwt', { session: false }),
-    (req, res, next) => {
-      let newBatch = req.body
-      //newBatch.authorId = req.account._id
-
-      Batch.create(newBatch)
-        .then((batch) => res.json(batch))
-        .catch((error) => next(error))
-    })
-
 app.use(users)
 app.use(sessions)
+app.use(batches)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
